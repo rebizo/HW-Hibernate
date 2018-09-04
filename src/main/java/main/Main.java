dd705fa20ade072c70074749f4a504e179ef8e88
gs.dzhura <dzhura.gs@minsvyazdnr.ru>
2018-09-04 15:29:09 +0300

    First commit

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,7 +3,7 @@ package main;
 import dbService.DBException;
 import dbService.DBService;
 import dbService.dataSets.UsersDataSet;
-
+//
 public class Main {
     public static void main(String[] args) {
         DBService dbService = new DBService();
