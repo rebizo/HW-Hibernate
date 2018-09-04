4763fd2bda9e3b134793db1bcba5d5cb3d6d166f
gs.dzhura <dzhura.gs@minsvyazdnr.ru>
2018-09-04 15:26:00 +0300

    First commit

---
+++ src/main/java/dbService/dao/UsersDAO.java
@@ -0,0 +1,45 @@
+package dbService.dao;
+
+import dbService.dataSets.UsersDataSet;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+public class UsersDAO {
+
+    private Session session;
+
+    public UsersDAO(Session session) {
+        this.session = session;
+    }
+
+    public UsersDataSet get(long id) throws HibernateException {
+        return (UsersDataSet) session.get(UsersDataSet.class, id);
+    }
+
+    /*    public long getUserId(String name) throws HibernateException {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<UsersDataSet> criteria = criteriaBuilder.createQuery(UsersDataSet.class);
+            Root<UsersDataSet> usersDataSetRoot = criteria.from(UsersDataSet.class);
+            return ((UsersDataSet)criteria.where(criteriaBuilder.equal(usersDataSetRoot.get("name"), name))).getId();
+        }*/
+    public long getUserId(String username) {
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<UsersDataSet> criteriaQuery = builder.createQuery(UsersDataSet.class); //создаем критерий
+        Root<UsersDataSet> root = criteriaQuery.from(UsersDataSet.class); //определяем переменную диапазона для FROM
+        criteriaQuery.select(root);  //какой тип результата запроса будет (можно поля задавать
+        criteriaQuery.where(builder.equal(root.get("name"), username)); //задаем where
+        Query<UsersDataSet> q = session.createQuery(criteriaQuery); //собственно сам запрос
+        UsersDataSet result = q.getSingleResult(); //получаем результат
+        return result.getId();
+    }
+
+
+    public long insertUser(String name) throws HibernateException {
+        return (Long) session.save(new UsersDataSet(name));
+    }
+}
---
+++ src/main/java/main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+import dbService.DBException;
+import dbService.DBService;
+import dbService.dataSets.UsersDataSet;
+
+public class Main {
+    public static void main(String[] args) {
+        DBService dbService = new DBService();
+        dbService.printConnectInfo();
+
+        try {
+            long userId = dbService.addUser("tully");
+            System.out.println("Added user id: " + userId);
+
+            UsersDataSet dataSet = dbService.getUser(userId);
+            System.out.println("User data set: " + dataSet);
+
+        } catch (DBException e) {
+            e.printStackTrace();
+        }
+    }
+}
